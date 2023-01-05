d30c725415386133d3c0c353aefadfde530c8546
GeorgePopaA <poplike14@yahoo.com>
2023-01-05 20:09:31 +0200

    I have added an interface which I have implemented in my 2 classes for demonstrating Open Closed Principle , Now my code is open for extensions due of created interface but closed for modification. Using a delegation of Shape interface we have applied "closed" from principle.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
-class Rectangle{
+interface Shape{
+    public double calculateArea();
+}
+
+class Rectangle implements Shape{
     public double length;
     public double width;
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+class Circle implements Shape{
+    public double radius;
+
+    @Override
+    public double calculateArea() {
+        return (22/7) * radius *radius;
+    }
 }
 
 class AreaCalculator{
-    public double calculateRectangleArea(Rectangle rectangle){
-        return rectangle.length * rectangle.width;
-    }
-
-    public double calculateCircleArea(Circle circle){
-        return (22/7) * circle.radius * circle.radius;
+    public double calculateShapeArea(Shape shape){
+        return shape.calculateArea();
     }
 }
 
-class Circle{
-    public double radius;
-}
+
 
 
 
